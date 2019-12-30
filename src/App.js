0a6018652621ef96ea319cc45b63ea3948ba90a3
ypablo <yfedora@gmail.com>
2019-12-30 17:51:59 +0000

    Init adv 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Layout from "./components/Layout/Layout"
 
 function App() {
   return (
     <div>
-
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
