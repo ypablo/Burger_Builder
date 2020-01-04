f4ae397d0c7d39432c5c42ee6fb88e96ee30c13b
Pawel Banas <Pawel.Banas@flexitricity.com>
2020-01-04 13:50:25 +0000

    Add order modal

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 //import Auxi from "../../hoc/Auxi"
 import Burger from "../../components/Burger/Burger"
 import BuildControls from "../../components/Burger/BuildControls/BuildControls"
+import Modal from "../../components/UI/Modal/Modal"
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary" 
 
 const INGREDIENT_PRICES = {
     Salad: 1,
@@ -72,6 +74,9 @@ export default class BurgerBuilder extends Component {
         }
         return (
             <div>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
                     ingredientAdded={this.addIngredienthandler} 
